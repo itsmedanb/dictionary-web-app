53a3190dba03cef09c99e964815a7d43682cf873
DB <96037500+itsmedanb@users.noreply.github.com>
2024-01-17 22:16:04 -0700

    js changed
    main app functionality completed.
    Js file need to be refactored to make code more dry.
    Css needs to be updated to include minor accent changes and hover effects.

--- index.js
+++ index.js
@@ -11,12 +11,15 @@ let playBtn = document.getElementById("play-btn")
 let selectFont = document.getElementById("select-font")
 let nounMeanings = ''
 let verbMeanings = ''
+let synonyms = document.getElementById("main-content-synonyms")
+let synonymsData = ''
 let verbExample = ''
 let hasNounMeanings = false
 let hasVerbMeanings = false
 let phoneticsAudio = ''
 let mainContent = document.getElementById("main-content")
 let optionsBar = document.getElementById("options-bar")
+
 mainContent.classList.add("hidden")
 optionsBar.style.marginTop = "40%"
 
@@ -25,6 +28,12 @@ function reset(){
     optionsBar.style.marginTop = "2rem"
 
     document.getElementById("audio").src=``
+    nounMeanings = ''
+    verbMeanings = ''
+    verbExample = ''
+    hasNounMeanings = false
+    hasVerbMeanings = false
+    phoneticsAudio = ''
 }
 
 function setMainContentWord(data){
@@ -82,6 +91,7 @@ function renderHTML(){
 }
 
 function setPhoneticsAudio(data){
+    phoneticsAudio = ''
     for(let i = 0; i < data[0].phonetics.length; i++){
         if(data[0].phonetics[i].audio != ''){
             phoneticsAudio = data[0].phonetics[i].audio
@@ -96,6 +106,23 @@ function setSourceLink(data){
     <img src="./assets/images/icon-new-window.svg" alt="" >
     `
 }
+
+function setSynonyms(data){
+    for(let i = 0; i < data[0].meanings.length; i++){
+        if(data[0].meanings[i].partOfSpeech == "noun"){
+            for(let j = 0; j < data[0].meanings[i].definitions.length; j++){
+                //data[0].meanings[i].synonyms[j].length > 0
+                if(data[0].meanings[i].synonyms[j]){
+                    // add to inner html with class of hidden, when clicking synonym word hide verb/verb meaning and display synonyms. synonyms button should turn into verb button and should click on verb button to hide synonyms and display verbs
+                    synonymsData += `
+                    <li>${data[0].meanings[i].synonyms[j]}</li>
+                    `
+                }
+            }  
+        }
+    }
+}
+
 function fetchDictionaryData(){
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchBarInput.value}`)
     .then(res => res.json())
@@ -104,21 +131,48 @@ function fetchDictionaryData(){
         setMainContentWord(data)
         setMainContentIPA(data)
         setMainContentNoun(data)
+        setSynonyms(data)
         setMainContentVerb(data)
         setPhoneticsAudio(data)
         setVerbExample()
         renderHTML()
         setSourceLink(data)
+
     })
+    
 }
 
 searchBarInput.addEventListener("change", function(){
     fetchDictionaryData()
-    playBtn.addEventListener("click",function(){
-        document.getElementById("audio").play()
-    })
+
+    
 })
 
+playBtn.addEventListener("click",function(){
+        
+    if(document.getElementById("audio").play() !== undefined){
+        document.getElementById("audio").play().then(function(){})
+        .catch(function(error) {
+            
+          });
+    }
+
+})
+
+synonyms.addEventListener("click", function(){
+    if(synonyms.textContent == "Synonyms"){
+        synonyms.textContent = "Verbs"
+        document.getElementById("main-content-verb").textContent = "synonyms"
+        document.getElementById("main-content-verb-meaning").textContent = ''
+        document.getElementById("main-content-verb-meaning-list").innerHTML = synonymsData
+        
+    }else if(synonyms.textContent == "Verbs"){
+        synonyms.textContent = "Synonyms"
+        document.getElementById("main-content-verb").textContent = "Verbs"
+        document.getElementById("main-content-verb-meaning").textContent = 'Meaning'
+        document.getElementById("main-content-verb-meaning-list").innerHTML = verbMeanings
+    }
+})
 
 selectFont.addEventListener("change", function(){
     console.log(selectFont.value)
@@ -134,21 +188,14 @@ selectFont.addEventListener("change", function(){
 })
 
 
+// synonyms.addEventListener("click", function(){
+//     if(synonyms.textContent == "Synonyms"){
+//         synonyms.textContent = "Verbs"
+//         document.getElementById("main-content-verb").textContent = "synonyms"
+//         document.getElementById("main-content-verb-meaning").textContent = ''
+//         document.getElementById("main-content-verb-meaning-list").innerHTML = synonymsData
+//     }
+//     if(synonyms.textContent == "Verbs"){
 
-
-
-
-
-
-
-
-
-
-
-
-//set audio to play on click
-
-
-
-
-
+//     }
+// })
